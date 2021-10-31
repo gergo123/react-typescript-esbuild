03abdcdd25d744d2ff4be3853f70dad8274b260f
Gergo N <csovis@gmail.com>
2021-10-31 13:20:20 +0100

    Add project files.

---
+++ react-typescript-esbuild/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace react_typescript_esbuild.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
