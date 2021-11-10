7e6bb27c5e746322e62ec2d6afb597823264da10
Gergo N <csovis@gmail.com>
2021-11-10 08:17:39 +0100

    move stories under src folder

--- react-typescript-esbuild/ClientApp/src/stories/counter.stories.tsx
+++ react-typescript-esbuild/ClientApp/src/stories/counter.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Counter from '../src/components/counter';
+import Counter from '../../src/components/counter';
 
 export default {
     title: 'Componens/Counter',
