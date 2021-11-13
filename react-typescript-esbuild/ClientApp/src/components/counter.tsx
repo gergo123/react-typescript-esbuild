548e15e3b983bc1f8cfa1c5b211f54e09b63f91e
Gergo N <csovis@gmail.com>
2021-11-13 20:00:10 +0100

    seperate css file for counter component

--- react-typescript-esbuild/ClientApp/src/components/counter.tsx
+++ react-typescript-esbuild/ClientApp/src/components/counter.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import ReactDOM from "react-dom";
-import "../index.css"
+import "./counter.css"
 
 const Counter = (props: { message: string }) => {
     const [count, setCount] = React.useState<number>(1);
