e8d0e0a5d623410b96a766d26eabd646a94df157
Gergo N <csovis@gmail.com>
2021-11-13 20:00:36 +0100

    more unit tests for counter component

--- react-typescript-esbuild/ClientApp/__tests__/components/counter.test.tsx
+++ react-typescript-esbuild/ClientApp/__tests__/components/counter.test.tsx
@@ -1,11 +1,30 @@
 // __tests__/fetch.test.js
 import React from 'react'
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { render, fireEvent, waitFor, screen, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Counter from 'components/counter';
 
-test('should display given string parameter', async () => {
-    render(<Counter message="Hello world" ></Counter>)
+beforeEach(() => {
+    render(<Counter message="Hello world" ></Counter>);
+})
+afterEach(cleanup)
 
+test('should display given string parameter', async () => {
     expect(screen.getByText('Hello world')).toBeVisible();
+})
+
+test('counter should be one', async () => {
+    expect(screen.getByText('Count: 1')).toBeVisible();
+})
+
+test('clicking button should increment counter', async () => {
+    expect(screen.getByText('Count: 1')).toBeVisible();
+    fireEvent.click(screen.getByText('Click me'))
+    expect(screen.getByText('Count: 2')).toBeVisible();
+    fireEvent.click(screen.getByText('Click me'))
+    expect(screen.getByText('Count: 3')).toBeVisible();
+    fireEvent.click(screen.getByText('Click me'))
+    fireEvent.click(screen.getByText('Click me'))
+    fireEvent.click(screen.getByText('Click me'))
+    expect(screen.getByText('Count: 6')).toBeVisible();
 })
