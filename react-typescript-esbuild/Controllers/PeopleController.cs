04d360b8572d6d6a82d40d323d456ff7e8d55e8a
Gergo N <csovis@gmail.com>
2021-12-30 18:14:01 +0100

    Controller and view setup for people list example

---
+++ react-typescript-esbuild/Controllers/PeopleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using react_typescript_esbuild.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace react_typescript_esbuild.Controllers
+{
+    public class PeopleController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var people = new PersonViewModel[] {
+                new PersonViewModel { Id = 1, Name = "Mark" }
+            };
+            return View(people);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] PersonViewModel[] people)
+        {
+            throw new Exception("Do whatever you feel like with the data");
+
+            return View(people);
+        }
+    }
+}
---
+++ react-typescript-esbuild/Models/PersonViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace react_typescript_esbuild.Models
+{
+    public class PersonViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
